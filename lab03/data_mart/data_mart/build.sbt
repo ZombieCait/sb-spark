1025f631aebba8a03b9a8253f226ae6991b72e41
zombie <caitzombie@gmail.com>
2022-07-31 19:03:17 +0500

    add lab3 after test on node

--- lab03/data_mart/data_mart/build.sbt
+++ lab03/data_mart/data_mart/build.sbt
@@ -1,12 +1,17 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.11.12"
+val sparkVersion = "2.4.5"
 
 lazy val root = (project in file("."))
   .settings(
     name := "data_mart"
   )
 
-libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "2.4.3"
-libraryDependencies += "org.elasticsearch" %% "elasticsearch-spark-20" % "7.6.2"
-libraryDependencies += "org.postgresql" % "postgresql" % "42.2.12"
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "com.datastax.spark" %% "spark-cassandra-connector" % "2.4.3",
+  "org.elasticsearch" %% "elasticsearch-spark-20" % "6.8.9",
+  "org.postgresql" % "postgresql" % "42.2.12"
+)
--- lab03/data_mart/data_mart/src/main/scala/data_mart.scala
+++ lab03/data_mart/data_mart/src/main/scala/data_mart.scala
@@ -4,86 +4,91 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.cassandra._
 import org.apache.spark.sql.SparkSession
 
-def get_age_category =  udf { (age: Int) =>
-  age match {
-    case x if ((x>=18) & (x<=24)) => "18-24"
-    case x if ((x>=25) & (x<=34)) => "25-34"
-    case x if ((x>=35) & (x<=44)) => "35-44"
-    case x if ((x>=45) & (x<=54)) => "45-54"
-    case _ => ">=55"
-  }
-}
-
-def get_domain =  udf { (str:String) => {
-  str.split("/")(0)
-  }
-}
-
 object data_mart{
-  val user_name = "ekaterina_patrakova"
-  val password = "KOIfK1gQ"
-  var spark =  SparkSession.builder.master("yarn").appName("Lab3_patrakova")
 
-  spark.conf.set("spark.cassandra.connection.host", "10.0.0.31")
-  spark.conf.set("spark.cassandra.connection.port", "9042")
-  spark.conf.set("spark.cassandra.output.consistency.level", "ANY")
-  spark.conf.set("spark.cassandra.input.consistency.level", "ONE")
+  def get_age_category =  udf { (age: Int) =>
+    age match {
+      case x if ((x>=18) & (x<=24)) => "18-24"
+      case x if ((x>=25) & (x<=34)) => "25-34"
+      case x if ((x>=35) & (x<=44)) => "35-44"
+      case x if ((x>=45) & (x<=54)) => "45-54"
+      case _ => ">=55"
+    }
+  }
 
-  //Visits from ElasticSearch
-  val esOptions =
-    Map(
-      "es.nodes" -> "10.0.0.31:9200",
-      "es.batch.write.refresh" -> "false",
-      "es.nodes.wan.only" -> "true"
-    )
+  def get_domain =  udf { (str:String) => {
+    str.split("/")(0)
+    }
+  }
 
-  var visits = spark.read.format("es").options(esOptions).load("visits")
-    .withColumn("shop",
-      concat(lit("shop_"),
-        regexp_replace(lower(col("category"))," |-", "_")))
+  def main(args: Array[String]): Unit = {
+    val user_name = "ekaterina_patrakova"
+    val password = "KOIfK1gQ"
+    var spark = SparkSession.builder.master("yarn")
+                      .appName("Lab3_patrakova")
+                      .getOrCreate()
 
-  visits = visits.groupBy("uid").pivot("shop").count()
+    spark.conf.set("spark.cassandra.connection.host", "10.0.0.31")
+    spark.conf.set("spark.cassandra.connection.port", "9042")
+    spark.conf.set("spark.cassandra.output.consistency.level", "ANY")
+    spark.conf.set("spark.cassandra.input.consistency.level", "ONE")
 
-  //Clients from cassandra
-  var clients = spark
-    .read
-    .format("org.apache.spark.sql.cassandra")
-    .options(Map("table" -> "clients", "keyspace" -> "labdata"))
-    .load()
-    .withColumn("age_cat", get_age_category(col("age")))
+    //Visits from ElasticSearch
+    val esOptions =
+      Map(
+        "es.nodes" -> "10.0.0.31:9200",
+        "es.batch.write.refresh" -> "false",
+        "es.nodes.wan.only" -> "true"
+      )
 
-  //Categories from PostgreSQL
-  val websites_categories = spark.read
-    .format("jdbc")
-    .option("url", "jdbc:postgresql://10.0.0.31:5432/labdata")
-    .option("dbtable", "domain_cats")
-    .option("user", user_name)
-    .option("password", password)
-    .option("driver", "org.postgresql.Driver")
-    .load()
-    .withColumn("category", concat(lit("web_"), col("category")))
+    var visits = spark.read.format("es").options(esOptions).load("visits")
+      .withColumn("shop",
+        concat(lit("shop_"),
+          regexp_replace(lower(col("category")), " |-", "_")))
 
-  // Weblogs from hdfs
-  var weblogs = spark.read.json("hdfs:///labs/laba03/weblogs.json")
-    .select(col("uid"), explode(col("visits")("url")).as("URL"))
-    .withColumn("domain", regexp_replace(get_domain(regexp_replace(col("URL"),
-      "^(https://)|(http://)", "")), "^(www.)", ""))
+    visits = visits.groupBy("uid").pivot("shop").count()
 
-  weblogs = weblogs.join(websites_categories, Seq("domain"), "left")
-                   .groupBy("uid").pivot("category").count()
+    //Clients from cassandra
+    var clients = spark
+      .read
+      .format("org.apache.spark.sql.cassandra")
+      .options(Map("table" -> "clients", "keyspace" -> "labdata"))
+      .load()
+      .withColumn("age_cat", get_age_category(col("age")))
 
-  var result = clients.join(visits, Seq("uid"), "left")
-    .join(weblogs,  Seq("uid"), "left")
-    .drop("age", "null")
+    //Categories from PostgreSQL
+    val websites_categories = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:postgresql://10.0.0.31:5432/labdata")
+      .option("dbtable", "domain_cats")
+      .option("user", user_name)
+      .option("password", password)
+      .option("driver", "org.postgresql.Driver")
+      .load()
+      .withColumn("category", concat(lit("web_"), col("category")))
 
-  //write result
-  result.write
-    .format("jdbc")
-    .option("url", "jdbc:postgresql://10.0.0.31:5432/"+user_name)
-    .option("dbtable", "clients")
-    .option("user", user_name)
-    .option("password", password)
-    .option("driver", "org.postgresql.Driver")
-    .mode("overwrite")
-    .save()
+    // Weblogs from hdfs
+    var weblogs = spark.read.json("hdfs:///labs/laba03/weblogs.json")
+      .select(col("uid"), explode(col("visits")("url")).as("URL"))
+      .withColumn("domain", regexp_replace(get_domain(regexp_replace(col("URL"),
+        "^(https://)|(http://)", "")), "^(www.)", ""))
+
+    weblogs = weblogs.join(websites_categories, Seq("domain"), "left")
+      .groupBy("uid").pivot("category").count()
+
+    var result = clients.join(visits, Seq("uid"), "left")
+      .join(weblogs, Seq("uid"), "left")
+      .drop("age", "null")
+
+    //write result
+    result.write
+      .format("jdbc")
+      .option("url", "jdbc:postgresql://10.0.0.31:5432/" + user_name)
+      .option("dbtable", "clients")
+      .option("user", user_name)
+      .option("password", password)
+      .option("driver", "org.postgresql.Driver")
+      .mode("overwrite")
+      .save()
+  }
 }
